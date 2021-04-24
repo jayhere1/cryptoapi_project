df11328dc8a3bcf8d18ac9929eb0a436669259cf
Jay <jayrulz99@gmail.com>
2021-04-24 21:03:55 +0100

    CSS update

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 This project uses the rapid vantage API to query data for Ethererum, converts to pandas dataframe, removes metadata, extracts the data for current date and displays this.
 
-Resources used: Python, Flask, gunicorn, Heroku
+Resources used: Python, Flask, Jinja2, gunicorn, Heroku
 
 Libraries: Pandas, Requests, Flask.
 
-Module: os, datetime.
+Modules: os, datetime.
--- app.py
+++ app.py
@@ -24,20 +24,21 @@ crypto_parameters = {
 
 @app.route('/')
 def home():
+    crypto_response = requests.get(url=CRYPTO_ENDPOINT, params=crypto_parameters)
+    crypto_response.raise_for_status()
+    crypto_data = crypto_response.json()
+
+    # current date and time
+    now = datetime.now()
+    date = now.strftime("%Y-%m-%d")
+
     try:
-        crypto_response = requests.get(url=CRYPTO_ENDPOINT, params=crypto_parameters)
-        crypto_response.raise_for_status()
-        crypto_data = crypto_response.json()
         # Converts to pandas dataframe and remove metadata
         crypto_df = pd.DataFrame(crypto_data["Time Series (Digital Currency Daily)"], index=None)
 
         # Change row names for the display
         crypto_df.index = ["Open (GBP)", "Open (USD)", "High (GBP)", "High (USD)", "Low (GBP)", "Low (USD)",
                            "Close (GBP)", "Close (USD)", "Volume", "Market cap (USD)"]
-
-        # current date and time
-        now = datetime.now()
-        date = now.strftime("%Y-%m-%d")
 
         # Extract data for current date
         crypto_endpoint = (crypto_df[f"{date}"])
